f87c77b2b582faf3aabb2dc1b9fae39f6127859e
noerrornowork <13058417431@163.com>
2019-03-03 15:17:18 +0800

    维护注册接口

--- router/users.js
+++ router/users.js
@@ -1,7 +1,7 @@
 // @login & @register
 const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 const User = require("../models/users");
 
@@ -11,21 +11,23 @@ router.get('/test', (req, res) => {
    })
 });
 
-// 注册
+// 注册接口
 router.post("/register", (req,res) => {
-    console.log(req.body);
+    // console.log(req.body);
 
     User.findOne({email: req.body.email})
         .then(user => {
             if(user) {
-                return res.status(400).json({email: "邮箱已被注册!"})
+                return res.status(500).json({email: "邮箱已被注册!"})
             }else {
                 const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
                     password: req.body.password
                 });
-
+                let salt = bcrypt.genSaltSync(10);
+                let hash = bcrypt.hashSync(newUser.password, salt);
+                newUser.password = hash;
                 newUser.save()
                     .then(user => res.json(user))
                     .catch(err => console.log(err));
